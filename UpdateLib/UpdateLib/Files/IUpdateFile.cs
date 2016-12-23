4f445fe3aebbd580a7c0bf9629dcb1fdb7f3f969
Matthias Beerens <beerens.matthias@hotmail.com>
2016-12-23 18:57:29 +0100

    Adding generic files

---
+++ UpdateLib/UpdateLib/Files/IUpdateFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MatthiWare.UpdateLib.Files
+{
+    public interface IUpdateFile
+    {
+        Version Version { get; }
+
+        void AddFile(IFile file);
+
+        void Generate();
+
+        IUpdateFile LoadFile(String path);
+    }
+}
--- UpdateLib/UpdateLib/Updater.cs
+++ UpdateLib/UpdateLib/Updater.cs
@@ -1,11 +1,52 @@
-﻿using System;
+﻿using MatthiWare.UpdateLib.Files;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MatthiWare.UpdateLib
 {
     public class Updater
     {
+        private String m_url;
+        private IUpdateFile m_updateFile;
+        private String m_temp;
+        private Assembly m_asm;
+
+        public Updater(String url)
+        {
+            m_url = url;
+            m_asm = Assembly.GetEntryAssembly();
+            m_temp = String.Concat(Path.GetTempPath(), m_asm.FullName, "/Updater/");
+        }
+
+
+
+        public async Task<bool> CheckForUpdates()
+        {
+            
+            return true;
+        }
+
+        private async Task<IUpdateFile> GetUpdateFileFromUrl(String url)
+        {
+            WebClient client = new WebClient();
+            return null;
+        }
+
+        private Version GetCurrentVersion()
+        {
+            return new Version(m_asm.ImageRuntimeVersion);
+        }
+
+        public void StartUpdate()
+        {
+
+        }
+
     }
 }
