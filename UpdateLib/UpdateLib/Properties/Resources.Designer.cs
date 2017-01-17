969ce70a0116cf74e33666e2d11ec10694c9c78a
Matthias Beerens <beerens.matthias@hotmail.com>
2017-01-17 01:07:13 +0100

    Progress animation added

--- UpdateLib/UpdateLib/Properties/Resources.Designer.cs
+++ UpdateLib/UpdateLib/Properties/Resources.Designer.cs
@@ -99,5 +99,15 @@ namespace MatthiWare.UpdateLib.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap status_working {
+            get {
+                object obj = ResourceManager.GetObject("status_working", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
