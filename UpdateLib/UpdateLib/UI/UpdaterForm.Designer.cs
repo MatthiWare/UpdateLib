73e14e51fcc98e32b92b2341bb22f9e8c3e4a9bf
Matthias Beerens <beerens.matthias@hotmail.com>
2017-01-19 17:51:55 +0100

    Adding intropart

--- UpdateLib/UpdateLib/UI/UpdaterForm.Designer.cs
+++ UpdateLib/UpdateLib/UI/UpdaterForm.Designer.cs
@@ -31,6 +31,8 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdaterForm));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.lblUpdateLibLink = new System.Windows.Forms.LinkLabel();
+            this.lblPoweredBy = new System.Windows.Forms.Label();
             this.btnUpdateCancel = new System.Windows.Forms.Button();
             this.lvItems = new System.Windows.Forms.ListView();
             this.clmnImg = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -40,8 +42,7 @@
             this.clmnExtra = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.imgList = new System.Windows.Forms.ImageList(this.components);
             this.label1 = new System.Windows.Forms.Label();
-            this.lblPoweredBy = new System.Windows.Forms.Label();
-            this.lblUpdateLibLink = new System.Windows.Forms.LinkLabel();
+            this.pbarUpdate = new System.Windows.Forms.ProgressBar();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,16 +53,38 @@
             this.panel1.Controls.Add(this.lblPoweredBy);
             this.panel1.Controls.Add(this.btnUpdateCancel);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 299);
+            this.panel1.Location = new System.Drawing.Point(0, 313);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(481, 51);
+            this.panel1.Size = new System.Drawing.Size(499, 51);
             this.panel1.TabIndex = 0;
+            // 
+            // lblUpdateLibLink
+            // 
+            this.lblUpdateLibLink.AutoSize = true;
+            this.lblUpdateLibLink.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUpdateLibLink.Location = new System.Drawing.Point(81, 18);
+            this.lblUpdateLibLink.Name = "lblUpdateLibLink";
+            this.lblUpdateLibLink.Size = new System.Drawing.Size(61, 15);
+            this.lblUpdateLibLink.TabIndex = 2;
+            this.lblUpdateLibLink.TabStop = true;
+            this.lblUpdateLibLink.Text = "UpdateLib";
+            this.lblUpdateLibLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblUpdateLibLink_LinkClicked);
+            // 
+            // lblPoweredBy
+            // 
+            this.lblPoweredBy.AutoSize = true;
+            this.lblPoweredBy.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPoweredBy.Location = new System.Drawing.Point(12, 18);
+            this.lblPoweredBy.Name = "lblPoweredBy";
+            this.lblPoweredBy.Size = new System.Drawing.Size(75, 15);
+            this.lblPoweredBy.TabIndex = 1;
+            this.lblPoweredBy.Text = "Powered by: ";
             // 
             // btnUpdateCancel
             // 
             this.btnUpdateCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnUpdateCancel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUpdateCancel.Location = new System.Drawing.Point(394, 14);
+            this.btnUpdateCancel.Location = new System.Drawing.Point(412, 14);
             this.btnUpdateCancel.Name = "btnUpdateCancel";
             this.btnUpdateCancel.Size = new System.Drawing.Size(75, 23);
             this.btnUpdateCancel.TabIndex = 0;
@@ -82,7 +105,7 @@
             this.clmnExtra});
             this.lvItems.Location = new System.Drawing.Point(12, 29);
             this.lvItems.Name = "lvItems";
-            this.lvItems.Size = new System.Drawing.Size(457, 257);
+            this.lvItems.Size = new System.Drawing.Size(475, 249);
             this.lvItems.SmallImageList = this.imgList;
             this.lvItems.TabIndex = 1;
             this.lvItems.UseCompatibleStateImageBehavior = false;
@@ -125,39 +148,28 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(9, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(94, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "Files to update";
             // 
-            // lblPoweredBy
+            // pbarUpdate
             // 
-            this.lblPoweredBy.AutoSize = true;
-            this.lblPoweredBy.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPoweredBy.Location = new System.Drawing.Point(12, 18);
-            this.lblPoweredBy.Name = "lblPoweredBy";
-            this.lblPoweredBy.Size = new System.Drawing.Size(75, 15);
-            this.lblPoweredBy.TabIndex = 1;
-            this.lblPoweredBy.Text = "Powered by: ";
-            // 
-            // lblUpdateLibLink
-            // 
-            this.lblUpdateLibLink.AutoSize = true;
-            this.lblUpdateLibLink.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUpdateLibLink.Location = new System.Drawing.Point(86, 18);
-            this.lblUpdateLibLink.Name = "lblUpdateLibLink";
-            this.lblUpdateLibLink.Size = new System.Drawing.Size(61, 15);
-            this.lblUpdateLibLink.TabIndex = 2;
-            this.lblUpdateLibLink.TabStop = true;
-            this.lblUpdateLibLink.Text = "UpdateLib";
-            this.lblUpdateLibLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblUpdateLibLink_LinkClicked);
+            this.pbarUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbarUpdate.Location = new System.Drawing.Point(12, 284);
+            this.pbarUpdate.Name = "pbarUpdate";
+            this.pbarUpdate.Size = new System.Drawing.Size(475, 23);
+            this.pbarUpdate.TabIndex = 3;
+            this.pbarUpdate.Value = 50;
             // 
             // UpdaterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(481, 350);
+            this.ClientSize = new System.Drawing.Size(499, 364);
+            this.Controls.Add(this.pbarUpdate);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lvItems);
             this.Controls.Add(this.panel1);
@@ -186,5 +198,6 @@
         private System.Windows.Forms.ImageList imgList;
         private System.Windows.Forms.LinkLabel lblUpdateLibLink;
         private System.Windows.Forms.Label lblPoweredBy;
+        private System.Windows.Forms.ProgressBar pbarUpdate;
     }
 }
