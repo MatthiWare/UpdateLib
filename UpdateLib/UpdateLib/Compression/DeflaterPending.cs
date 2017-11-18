61aa2866c8b8a99a4c38f3c66de5322cc105d71a
Matthias Beerens <beerens.matthias@hotmail.com>
2017-11-18 15:10:30 +0100

    Added Deflater/Inflater

---
+++ UpdateLib/UpdateLib/Compression/DeflaterPending.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MatthiWare.UpdateLib.Compression
+{
+    /// <summary>
+    /// This class stores the pending output of the Deflater.
+    /// 
+    /// author of the original java version : Jochen Hoenicke
+    /// </summary>
+    public class DeflaterPending : PendingBuffer
+    {
+        /// <summary>
+        /// Construct instance with default buffer size
+        /// </summary>
+        public DeflaterPending() : base(DeflaterConstants.PENDING_BUF_SIZE)
+        {
+        }
+    }
+}
