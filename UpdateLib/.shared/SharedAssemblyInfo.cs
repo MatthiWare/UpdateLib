8878738432f9247a2f7cce27dc7fa320264bd990
Matthias Beerens <beerens.matthias@hotmail.com>
2017-12-26 13:37:52 +0100

    Add SharedAssemblyInfo

---
+++ UpdateLib/.shared/SharedAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("MatthiWare")]
+[assembly: AssemblyProduct("UpdateLib")]
+[assembly: AssemblyCopyright("Copyright © MatthiWare 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
--- UpdateLib/UpdateLib.Generator/Properties/AssemblyInfo.cs
+++ UpdateLib/UpdateLib.Generator/Properties/AssemblyInfo.cs
@@ -1,35 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UpdateLib Generator")]
-[assembly: AssemblyDescription("Update Generator")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MatthiWare")]
-[assembly: AssemblyProduct("UpdateLib")]
-[assembly: AssemblyCopyright("Copyright © MatthiWare 2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5194ff71-49f6-4adb-9b0e-4beb418dc6f3")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyDescription("Update Generator for UpdateLib")]
